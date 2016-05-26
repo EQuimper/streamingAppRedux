94072494870ea30774cab893c357cbfc1c79537a
EQuimper <quimperemanuel@gmail.com>
2016-05-26 17:18:15 -0600

    Setup the some routes and the NoMatch one

--- src/js/routes/index.js
+++ src/js/routes/index.js
@@ -4,12 +4,16 @@ import { Router, Route, IndexRoute } from 'react-router';
 import { history } from '../store/configureStore';
 
 import App from '../containers/App';
-import Home from '../components/Home';
+import Home from '../components/layout/Home';
+import Games from '../components/layout/Games';
+import NoMatch from '../components/layout/NoMatch';
 
 export default (
 	<Router history={history}>
 		<Route path="/" component={App}>
 			<IndexRoute component={Home} />
+			<Route path="games" component={Games}/>
+			<Route path="*" component={NoMatch}/>
 		</Route>
 	</Router>
 );
