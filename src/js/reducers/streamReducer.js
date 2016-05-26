6b2a599ae3578ac547ed381e2e8cc3970ec59b04
EQuimper <quimperemanuel@gmail.com>
2016-05-26 01:20:43 -0600

    Setup streamReducer with Promise

--- src/js/reducers/streamReducer.js
+++ src/js/reducers/streamReducer.js
@@ -1,15 +1,29 @@
 import * as types from '../constants/';
 
-const initialStream = {
-	isFetching: false,
-	items: []
-};
+// const initialStream = {
+// 	isFetching: false,
+// 	items: []
+// };
 
-export const streamReducer = (state = [], action) => {
+const streamReducer = (streams = { loaded: false }, action) => {
 	switch (action.type) {
-		case types.RECEIVE_STREAMS:
-			return state;
+		case `${types.RECEIVE_STREAMS}_PENDING`:
+			return {};
+		case `${types.RECEIVE_STREAMS}_FULFILLED`:
+			return {
+				streams: action.payload,
+				err: null,
+				loaded: true
+			};
+		case `${types.RECEIVE_STREAMS}_REJECTED`:
+			return {
+				streams: null,
+				err: action.payload,
+				loaded: true
+			};
 		default:
-			return state;
+			return streams;
 	}
 };
+
+export default streamReducer;
