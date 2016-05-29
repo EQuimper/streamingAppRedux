f4005994ed20484f0b355ecb0341c09f318fa685
EQuimper <quimperemanuel@gmail.com>
2016-05-29 13:35:05 -0600

    Setup the store and the route + add the routeAction for bound all the streams

---
+++ src/js/actions/routeActions.js
@@ -0,0 +1,5 @@
+import {
+	reqStreams
+} from './streamActions';
+/*>>>>>>=============================================<<<<<<*/
+export const boundAllStreams = (nextState, replaceState) => reqStreams(nextState);
