53583bd9eb69033ec00708a6d5764e21c975e15a
EQuimper <quimperemanuel@gmail.com>
2016-05-31 16:01:36 -0600

    Setup the chat component for render the twitch iFrame + can toggle it

---
+++ src/js/components/TwitchChat.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Toggle from 'material-ui/Toggle';
+/*>>>>>>=============================================<<<<<<*/
+
+const styles = {
+    toggle: {
+        marginBottom: 16
+    }
+};
+
+const ChatIframe = ({ name }) => {
+    return (
+        <iframe
+            src={`https://www.twitch.tv/${name}/chat?popout=`}
+            frameborder="0"
+            scrolling="no"
+            height="500"
+            width="350">
+        </iframe>
+    );
+};
+
+class TwitchChat extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            showChat: false
+        };
+        this.showChat = this.showChat.bind(this);
+    }
+
+    showChat(e) {
+        this.setState({ showChat: !this.state.showChat });
+    }
+
+    render() {
+        const { name } = this.props;
+        return (
+            <div>
+                <Toggle
+                    label="Chat"
+                    labelPosition="right"
+                    defaultToggled={false}
+                    onClick={this.showChat}
+                    style={styles.toggle}
+                />
+                { this.state.showChat ? <ChatIframe name={name} /> : <h1>Why no chat ?</h1> }
+            </div>
+        );
+    }
+}
+;
+
+export default TwitchChat;
+
+
