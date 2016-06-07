35ee74fa4abb65d68cfe22d6b9775c25e23db5c2
EQuimper <quimperemanuel@gmail.com>
2016-06-07 11:02:10 -0600

    Make some change for the gh-pages

--- server.js
+++ server.js
@@ -1,48 +1,48 @@
-//const webpack = require('webpack');
-//const WebpackDevServer = require('webpack-dev-server');
-///*>>>>>>=============================================<<<<<<*/
-//const config = require('./webpack.dev.config.js');
-///*>>>>>>=============================================<<<<<<*/
-//const PORT = process.env.PORT || 3000;
-//
-//new WebpackDevServer(webpack(config), {
-//	publicPath: config.output.publicPath,
-//	hot: true,
-//	historyApiFallback: true
-//}).listen(PORT, 'localhost', (err, result) => {
-//	if (err) {
-//		return console.log(err);
-//	}
-//
-//	console.log(`Listening on port ${PORT}`);
-//});
-
-const path = require('path');
 const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+/*>>>>>>=============================================<<<<<<*/
 const config = require('./webpack.dev.config.js');
-const express = require('express');
-const app = express();
-const compiler = webpack(config);
-
-
-app.use(require('webpack-dev-middleware')(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
-}));
-
-app.use(require('webpack-hot-middleware')(compiler));
-
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
+/*>>>>>>=============================================<<<<<<*/
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, 'localhost', (err) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
+new WebpackDevServer(webpack(config), {
+	publicPath: config.output.publicPath,
+	hot: true,
+	historyApiFallback: true
+}).listen(PORT, 'localhost', (err, result) => {
+	if (err) {
+		return console.log(err);
+	}
 
-    console.log(`Listening at http://localhost:${PORT}`);
+	console.log(`Listening on port ${PORT}`);
 });
+//
+//const path = require('path');
+//const webpack = require('webpack');
+//const config = require('./webpack.dev.config.js');
+//const express = require('express');
+//const app = express();
+//const compiler = webpack(config);
+//
+//
+//app.use(require('webpack-dev-middleware')(compiler, {
+//    noInfo: true,
+//    publicPath: config.output.publicPath
+//}));
+//
+//app.use(require('webpack-hot-middleware')(compiler));
+//
+//app.get('*', function(req, res) {
+//    res.sendFile(path.join(__dirname, 'index.html'));
+//});
+//
+//const PORT = process.env.PORT || 3000;
+//
+//app.listen(PORT, 'localhost', (err) => {
+//    if (err) {
+//        console.log(err);
+//        return;
+//    }
+//
+//    console.log(`Listening at http://localhost:${PORT}`);
+//});
