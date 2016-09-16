0cb0041fa726718796c7e9d53c4cccd10b9e6472
EQuimper <quimperemanuel@gmail.com>
2016-09-15 21:11:47 -0400

    Refactor all the code and make it work with the new api

--- src/js/actions/featureStreamAction.js
+++ src/js/actions/featureStreamAction.js
@@ -1,11 +1,12 @@
 import request from "superagent";
 import * as types from "../constants/";
+import API_KEY from '../../../server/config/api_key';
 
-export const reqFeatureStreams = (featured) => {
+export const reqFeatureStreams = featured => {
 
   const promise = new Promise((resolve, reject) => {
     request
-      .get(`https://api.twitch.tv/kraken/streams/featured?limit=48&offset=0`)
+      .get(`https://api.twitch.tv/kraken/streams/featured?limit=48&offset=0&client_id=${API_KEY}`)
       .end((err, res) => {
         if (err) {
           reject(err);
@@ -16,7 +17,7 @@ export const reqFeatureStreams = (featured) => {
   });
 
   return {
-    type:    types.RECEIVE_FEATURES_STREAMS,
+    type: types.RECEIVE_FEATURES_STREAMS,
     payload: promise
   };
 
